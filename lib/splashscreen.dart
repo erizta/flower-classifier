e6a8431a2bbd626e34caa43deefec3dc44f02d50
erizta <eriztaalifad@gmail.com>
2020-11-02 08:41:34 +0700

    commit

---
+++ lib/splashscreen.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:splashscreen/splashscreen.dart';
+
+import 'home.dart';
+
+class MySplash extends StatefulWidget {
+  @override
+  _MySplashState createState() => _MySplashState();
+}
+
+class _MySplashState extends State<MySplash> {
+  @override
+  Widget build(BuildContext context) {
+    return SplashScreen(
+      seconds: 2,
+      navigateAfterSeconds: Home(),
+      title: Text(
+        'Dog and Cat',
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 30,
+          color: Color(0XFFE99600),
+        ),
+      ),
+      image: Image.asset(
+        'assets/cat.png',
+      ),
+      backgroundColor: Colors.black,
+      photoSize: 50,
+      loaderColor: Color(0XFFEEDA28),
+    );
+  }
+}
